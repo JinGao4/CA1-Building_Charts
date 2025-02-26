09cf231bfe6d4278812e1993a7bc09bf4affc77a
Jing Gao <N00231358@iadt.ie>
2025-02-26 10:25:44 +0000

    change the line chart

--- classes/LineChart.js
+++ classes/LineChart.js
@@ -33,44 +33,48 @@ class LineChart{
         this.numTicks = 5;
     }
 
-    render(){// this is a method that will render the bars in the chart
+    render(){ // this is a method that will render the bars in the chart
         push();
-	        translate(this.chartPosX, this.chartPosY);
-
+            translate(this.chartX, this.chartY);
+    
             push();
-            translate(this.margin, 0);
-
+            translate(this.margin, 0); // Adding margin for proper alignment
+    
             for (let i = 0; i < this.data.length; i++) {
                 push();
-                    translate((gap + this.barWidth * this.yValues.length) * i, 0);
-
+                    // This will position each group of bars for each x value
+                    translate((this.gap + this.barWidth * this.yValues.length) * i, 0);
+    
                     for (let j = 0; j < this.yValues.length; j++) {
                         noStroke();
                         fill(this.barColours[j % 3]);
-                        rect(this.barWidth * j, 0, this.barWidth, -this.data[i][this.yValues[j]] * scaler);
-
-                        fill(this.axisTextColour);
+    
+                        // Drawing the bars (rectangles)
+                        let barHeight = this.data[i][this.yValues[j]] * this.scaler;
+                        rect(this.barWidth * j, 0, this.barWidth, -barHeight);
+    
+                        // Displaying the text (data value) above each bar
+                        fill(this.textColour);
                         noStroke();
-                        textAlign(LEFT, CENTER);
+                        textAlign(CENTER, BOTTOM); // Centering the text above the bars
                         textSize(12);
-                        push();
-                        translate(this.barWidth, 10);
-                        rotate(60);
-                        text(this.data[i][this.xValue], 0, 0);
-                        pop();
+    
+                        // Display the value for each bar above it
+                        text(this.data[i][this.yValues[j]], this.barWidth * j + this.barWidth / 2, -barHeight - 5); // Positioning text above the bar
                     }
                 pop();
             }
             pop();
-
+    
             noFill();
             stroke(this.axisColour);
             strokeWeight(this.axisThickness);
             line(0, 0, 0, -this.chartHeight);
-            line(0, 0, this.chartWidth, 0);
-
+            line(0, 0, this.chartWidth, 0);  
+    
         pop();
     }
+    
 
     
 }
